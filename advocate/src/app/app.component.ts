09f40602c0e145289a64b7749628938993ed53ec
sudonym-i <isickln08@gmail.com>
2025-04-17 14:09:51 -0600

    finished styling for front end

--- advocate/src/app/app.component.ts
+++ advocate/src/app/app.component.ts
@@ -1,5 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginModalComponent } from './login-modal/login-modal.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  constructor(private dialog: MatDialog) {}
+
+  openLoginModal() {
+    this.dialog.open(LoginModalComponent, {
+      width: '300px',
+    });
+  }
+}
 
 @Component({
   selector: 'app-root',
